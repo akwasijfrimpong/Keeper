aa4fb474bf7bfc75ec4725854006adc69cac5514
akwasijfrimpong <akwasijfrimpong@gmail.com>
2024-07-18 16:20:41 +1000

    Keeper app Udemy Tutorial

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Header from "./components/Header";
+import Note from "./components/Note";
+import Footer from "./components/Footer";
+
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Note />
+      <Footer />
+    </div>
+  );
+}
+export default App;
---
+++ src/components/Note.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function Notes(){
+    return(<div className ="note">
+        <h1 > This is note title</h1>
+        <p> this is note content </p>
+    </div>)
+};
+
+export default Notes;
